80d19ad02c56fc4e37a0bba107a65f90696e25c8
Greg Fielding <greg@fieldingdesigngroup.com>
2021-06-25 11:06:05 -0700

    twilio

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,59 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp();
+const twilio = require('twilio');
+const db = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const accountSid = 'ACae6e454d50d65803615c3949a0a32c95';
+const authToken  = '75fb6e98a93d5973bbb25b5cc720e442';
+const client = new twilio(accountSid, authToken);
+const twilioNumber = '+14158765604'
+
+
+
+
+exports.newVerify = functions.firestore
+  .document('users/{id}').onUpdate((change, context) => {
+  const afterData = change.after.data()
+  const beforeData = change.before.data()
+
+  if (afterData.phone == beforeData.phone) {
+  	return null
+  } else {
+	  let phone = afterData.phone.replace(/[^0-9]+/g, "");
+	  if ( !validE164(phone) ) {
+	    throw new Error('number must be E164 format!')
+	  }
+	  return client.verify.services('VA9bd97992cc1a24a4bbcb41864727973f')
+    	 .verifications
+       .create({to: `+1` + phone, channel: 'sms'})
+       .then(verification => console.log(verification.status));
+  }
+})
+
+exports.newVerifyCheck = functions.firestore
+  .document('users/{id}').onUpdate((change, context) => {
+  const afterData = change.after.data()
+  const beforeData = change.before.data()
+
+  let phone = afterData.phone.replace(/[^0-9]+/g, "");
+  let code = afterData.code
+  let userId = afterData.id
+
+  if (afterData.code == beforeData.code) {
+  	return null
+  } else {
+  	return client.verify.services('VA9bd97992cc1a24a4bbcb41864727973f')
+      .verificationChecks
+      .create({to: `+1` + phone, code: code})
+      .then(verification_check => 
+      	admin.firestore().collection('users').doc(userId).update({
+      		phoneVerified: verification_check.status
+      	})
+     );
+  }
+})
+
+function validE164(num) {
+  return /^\+?[1-9]\d{1,14}$/.test(num)
+}
--- src/components/Profile/ProfileContact.vue
+++ src/components/Profile/ProfileContact.vue
@@ -21,9 +21,47 @@
       <input type="email" placeholder="" v-model.trim="userProfile.email" id="email" @change="updateEmail()"/>
     </div>
     <div class="mb-3">
-      <label for="phone">Cell Phone:</label>
-      <input type="phone" placeholder="" v-model.trim="userProfile.phone" v-mask="'(###) ###-####'" placeholder="(999) 999-9999" id="phone" @change="updateProfile()"/>
+    	<div class="flex align-center justify-space-between">
+	      <label for="phone">Cell Phone:</label>
+	      <span v-if="userProfile.phoneVerified == 'approved'" class="caption mb-2">verified <i style="color: green" class="fas fa-check"></i></span>
+	    </div>
+      <input type="phone" placeholder="" v-model.trim="userProfile.phone" v-mask="'(###) ###-####'" placeholder="(999) 999-9999" id="phone" @keyup="showTest = true"/>
+
+      <!-- Button when number added or changed -->
+      <transition name="fade">
+	      <button class="btn btn__primary mt-3" v-if="showTest" @click.prevent="updatePhone">
+	        Get Verification Code
+	      </button>
+	    </transition>
+
+	    <!-- Verify Code -->
+      <transition name="fade">
+        <div v-if="showVerification">
+        	<label for="code">Verification Code:</label>
+        	<input type="text" id="code" v-model.trim="code" v-mask="'######'" placeholder="######" required @keyup="showVerifyButton = true" />
+        </div>
+      </transition>
+
+      <!-- Button When Verify Code Added -->
+      <transition name="fade">
+        <button class="btn btn__primary mt-3" v-if="showVerifyButton" @click.prevent="verify">Verify</button>
+      </transition>
+
+      <!-- Button When Verify Error -->
+      <transition name="fade">
+        <div v-if="userProfile.phoneVerified && userProfile.phoneVerified == 'pending'">
+          <button class="btn btn__primary mt-3" @click.prevent="verify">Please check your code and try again</button>
+        </div>
+      </transition>
+
+      <transition name="fade">
+        <div v-if="error">
+          <p>{{error}}</p>
+        </div>
+      </transition>
+
     </div>
+    
     <div class="mb-3">
       <label for="dob">Date of Birth:</label>
       <input type="date" placeholder="" v-model.trim="userProfile.dob" id="dob" @change="updateProfile()"/>
@@ -32,8 +70,20 @@
 </template>
 
 <script>
+const fb = require('../../firebaseConfig.js')
+
 export default {
   props: ['userProfile', 'emailVerified'],
+  data: () => ({
+  	showTest: false,
+  	showVerification: false,
+    verified: false,
+    showVerify: false,
+    showVerifyButton: false,
+    verifyError: false,
+    code: '',
+    error: '',
+  }),
   methods: {
     updateProfile(){
     	let userProfile = this.userProfile
@@ -45,7 +95,36 @@ export default {
     },
     resendVerification() {
     	this.$store.dispatch('resendVerificationEmail')
-    }
+    },
+    updatePhone () {
+    	let userProfile = this.userProfile
+    	this.$store.dispatch('updateUser', userProfile)
+    	setTimeout(() => {
+          this.showTest = false
+          this.showVerification = true
+      }, 2000)
+    },
+    unVerify: function (e) {
+    	let userProfile = this.userProfile
+    	this.showVerification = false
+    	this.code = ''
+    	this.showVerification = false
+    	this.$store.dispatch('updateUser', {
+    		phoneVerified: null
+    	})
+    },
+    verify: function () {
+    	this.error = ''
+    	this.showTest = false
+    	this.$store.dispatch('updateUser', {
+    		code: this.code
+    	})
+      setTimeout(() => {
+        this.showVerifyButton = false
+        this.showVerification = false
+        this.code = ''
+      }, 5000)
+    },
   }
 }
 </script>
